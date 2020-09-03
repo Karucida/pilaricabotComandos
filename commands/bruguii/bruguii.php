6060dfc2bef79f35e3279c0b23d8d7fcd37e15cd
MrMarble <4268580+MrMarble@users.noreply.github.com>
2020-09-03 18:34:19 +0200

    Agrega el comando !bruguii

---
+++ commands/bruguii/bruguii.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * Responde a !bruguii con @bruguii hdp
+ *
+ * @author MrMarble
+ */
+class bruguii extends command{
+
+    public function doMagic($textOnCommand = null) {
+        $text="@bruguii hdp";
+        $this->setText($text);
+    }
+
+}
